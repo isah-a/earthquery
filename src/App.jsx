f1532e46fdb003be9529a7ea94e5a1dee05a667b
Isah <isahabdul16@gmail.com>
2025-08-25 19:43:06 +0100

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import ChatCanvas from './components/ChatCanvas';
-import History from './components/History';
 import NavBar from './components/NavBar';
-import Footer from './components/Footer'; // ✅ Import the Footer
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -15,7 +14,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/demo" element={<ChatCanvas />} />
-            <Route path="/docs" element={<Documentation />} />
+            {/* <Route path="/docs" element={<Documentation />} /> */}
           </Routes>
         </main>
         <Footer />
